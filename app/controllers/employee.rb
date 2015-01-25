a5e8557b38000c0ccbc5815eef0c1b5bbddaaaed
rjspencer <rjspencer82@gmail.com>
2015-01-25 22:23:32 +0000

    employee controller

---
+++ app/controllers/employee.rb
@@ -0,0 +1,36 @@
+# LIST ALL
+get '/employee' do
+  @employees = Employee.all
+  @employee = Employee.new
+  @employee.address = Address.new
+  erb :"employee/list"
+end
+
+# CREATE
+post '/employee/new' do
+  @employee = Employee.create(params[:employee])
+  @address = Address.create(params[:address])
+  @employee.address = @address
+  redirect '/employee'
+end
+
+# EDIT
+get '/employee/:id' do
+  @employee = Employee.find(params[:id])
+  erb :"employee/edit"
+end
+
+# UPDATE
+post '/employee/:id' do
+  @employee = Employee.find(params[:id])
+  @employee.update(params[:employee])
+  @employee.address.update(params[:address])
+  redirect '/employee'
+end
+
+# DESTROY
+post '/employee/delete/:id' do
+  @employee = Employee.find(params[:id])
+  @employee.destroy
+  redirect '/employee'
+end
